239754665b3bd5c8078b0cb2f4c35a48cd254a40
0M0Syndrom3 <arturoyakawa5@gmail.com>
2025-04-02 14:13:51 -0300

    Create exercicio2.cs

---
+++ src/exercicio2.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int quant;
+        float preco, total;
+        
+        Console.WriteLine("Digite a quantidade do produto que deseja: ");
+        quant = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("Digite o preco do produto que deseja: ");
+        preco = Convert.ToSingle(Console.ReadLine());
+        
+        total = quant*preco;
+        
+        Console.WriteLine("O valor total vai ser de: R$" + total);
+    }
+}
